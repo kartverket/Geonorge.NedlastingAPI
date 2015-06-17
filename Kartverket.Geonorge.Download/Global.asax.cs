01b11c30227b2b551715743138bd130c4c8e3eef
Benjamin Dehli <benjamin@arkitektum.no>
2015-06-17 13:20:06 +0200

    Add JSON query string in http request

--- Kartverket.Geonorge.Download/Global.asax.cs
+++ Kartverket.Geonorge.Download/Global.asax.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -18,6 +19,7 @@ namespace Kartverket.Geonorge.Download
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.MediaTypeMappings.Add(new QueryStringMapping("json", "true", "application/json"));
         }
     }
 }
