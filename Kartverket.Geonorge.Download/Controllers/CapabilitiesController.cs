40bac8e2a7c8fac5e96c2a202c61e983ff4ac4f4
Dag Olav <dagolav@arkitektum.no>
2015-06-17 13:13:10 +0200

    Enable cors api

--- Kartverket.Geonorge.Download/Controllers/CapabilitiesController.cs
+++ Kartverket.Geonorge.Download/Controllers/CapabilitiesController.cs
@@ -6,9 +6,11 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace Kartverket.Geonorge.Download.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class CapabilitiesController : ApiController
     {
         /// <summary>
