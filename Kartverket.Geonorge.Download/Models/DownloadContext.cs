bbcd0361ed079eba51777918c4972bb38c24ad21
Dag Olav <dagolav@arkitektum.no>
2015-06-19 08:23:18 +0200

    Remove unused code

--- Kartverket.Geonorge.Download/Models/DownloadContext.cs
+++ Kartverket.Geonorge.Download/Models/DownloadContext.cs
@@ -17,19 +17,5 @@ namespace Kartverket.Geonorge.Download.Models
             //prevents EF from attempting to create the database or generate migrations
             Database.SetInitializer<DownloadContext>(null);
         }
-
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            //modelBuilder.Entity<CapabilitiesType>().ToTable("Dataset");
-
-            //modelBuilder.Entity<CapabilitiesType>().Property(t => t.supportsAreaSelection).HasColumnName("supportsAreaSelection");
-            //modelBuilder.Entity<CapabilitiesType>().Property(t => t.supportsFormatSelection).HasColumnName("supportsFormatSelection");
-            //modelBuilder.Entity<CapabilitiesType>().Property(t => t.supportsPolygonSelection).HasColumnName("supportsPolygonSelection");
-            //modelBuilder.Entity<CapabilitiesType>().Property(t => t.supportsProjectionSelection).HasColumnName("supportsProjectionSelection");
-
-            modelBuilder.Entity<ProjectionType>().HasKey(p => p.code);
-            modelBuilder.Entity<AreaType>().HasKey(a => a.name);
-
-        }
     }
 }
