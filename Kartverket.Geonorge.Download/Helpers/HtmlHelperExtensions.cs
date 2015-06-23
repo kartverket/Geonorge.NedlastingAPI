49ab960cde21dd60e8a6cfcaa392e7a94a41c4d8
Dag Olav <dagolav@arkitektum.no>
2015-06-23 11:01:10 +0200

    Added BuildVersionNumber etc

---
+++ Kartverket.Geonorge.Download/Helpers/HtmlHelperExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Kartverket.Geonorge.Download.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+
+namespace Kartverket.Geonorge.Download.Helpers
+{
+    public static class HtmlHelperExtensions
+    {
+        public static string ApplicationVersionNumber(this HtmlHelper helper)
+        {
+            string versionNumber = WebConfigurationManager.AppSettings["BuildVersionNumber"];
+            return versionNumber;
+        }
+    }
+}
