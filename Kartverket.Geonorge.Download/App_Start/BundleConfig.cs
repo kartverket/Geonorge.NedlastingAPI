a47a1b8e732b8f64627b2a1e4252b3cc102b2a5e
Benjamin Dehli <benjamin@arkitektum.no>
2016-05-30 15:49:32 +0200

    Add Bower

--- Kartverket.Geonorge.Download/App_Start/BundleConfig.cs
+++ Kartverket.Geonorge.Download/App_Start/BundleConfig.cs
@@ -8,7 +8,15 @@ namespace Kartverket.Geonorge.Download
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            
+            bundles.Add(new StyleBundle("~/Content/bower_components/kartverket-felleskomponenter/assets/css/styles").Include(
+               "~/Content/bower_components/kartverket-felleskomponenter/assets/css/vendor.min.css",
+               "~/Content/bower_components/kartverket-felleskomponenter/assets/css/main.min.css"
+               ));
+
+            bundles.Add(new ScriptBundle("~/Content/bower_components/kartverket-felleskomponenter/assets/js/scripts").Include(
+               "~/Content/bower_components/kartverket-felleskomponenter/assets/js/vendor.min.js",
+               "~/Content/bower_components/kartverket-felleskomponenter/assets/js/main.min.js"
+               ));
         }
     }
 }
