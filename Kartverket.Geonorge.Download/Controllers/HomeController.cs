1e31b6d9d7a1b48a5086fe2889f05e531c209bb8
Veronika Andersen <veronika@arkitektum.no>
2017-11-09 11:10:10 +0100

    Add button to change language

--- Kartverket.Geonorge.Download/Controllers/HomeController.cs
+++ Kartverket.Geonorge.Download/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Kartverket.Geonorge.Download.Helpers;
 
 namespace Kartverket.Geonorge.Download.Controllers
 {
@@ -16,6 +17,28 @@ namespace Kartverket.Geonorge.Download.Controllers
             ViewBag.Title = "Download";
 
             return View();
+        }
+
+        public ActionResult SetCulture(string culture, string returnUrl)
+        {
+            // Validate input
+            culture = CultureHelper.GetImplementedCulture(culture);
+            // Save culture in a cookie
+            HttpCookie cookie = Request.Cookies["_culture"];
+            if (cookie != null)
+                cookie.Value = culture;   // update cookie value
+            else
+            {
+                cookie = new HttpCookie("_culture");
+                cookie.Value = culture;
+                cookie.Expires = DateTime.Now.AddYears(1);
+            }
+            Response.Cookies.Add(cookie);
+
+            if (!string.IsNullOrEmpty(returnUrl))
+                return Redirect(returnUrl);
+            else
+                return RedirectToAction("Index");
         }
 
         protected override void OnException(ExceptionContext filterContext)
