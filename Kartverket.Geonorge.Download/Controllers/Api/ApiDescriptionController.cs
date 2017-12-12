6e958fcbde17978c899a3db62868218830ef8082
Henning Jensen <henning@arkitektum.no>
2017-12-12 10:57:15 +0100

    Refactor capabilities controllers

---
+++ Kartverket.Geonorge.Download/Controllers/Api/ApiDescriptionController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Net.Mime;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Geonorge.NedlastingApi.V3;
+using Kartverket.Geonorge.Download.Models;
+
+namespace Kartverket.Geonorge.Download.Controllers.Api
+{
+    public class ApiDescriptionController : ApiController
+    {
+        [HttpGet]
+        [Route("api")]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public IHttpActionResult Index()
+        {
+            return Ok(ApiDescription());
+        }
+
+        [HttpGet]
+        [Route("api/capabilities")]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public IHttpActionResult CapabilitiesIndex()
+        {
+            if (HtmlIsFirstElementOfAcceptHeader())
+                return Redirect(UrlToHelpPages());
+
+            return Ok(ApiDescription());
+        }
+
+        private string UrlToHelpPages()
+        {
+            return Request.RequestUri.GetLeftPart(UriPartial.Authority) + "/Help/Api/GET-api-capabilities-metadataUuid";
+        }
+
+        private bool HtmlIsFirstElementOfAcceptHeader()
+        {
+            return Request.Headers.Accept.First().MediaType.Equals(MediaTypeNames.Text.Html);
+        }
+
+        private static VersionResponseType ApiDescription()
+        {
+            var capabilityLinksCreator = new CapabilityLinksCreator();
+            var apiVersionDescription = new VersionResponseType
+            {
+                version = capabilityLinksCreator.GetDefaultApiVersion(),
+                _links = capabilityLinksCreator.CreateLinks().ToArray()
+            };
+            return apiVersionDescription;
+        }
+    }
+}
