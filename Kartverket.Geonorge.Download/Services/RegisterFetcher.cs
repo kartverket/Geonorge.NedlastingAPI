616723c15fc63ebbee7ca95c5e3dc9f5b40edbea
Dag Olav Dahle <dagolav@arkitektum.no>
2018-01-15 12:55:44 +0100

    Handle old county and municipality

--- Kartverket.Geonorge.Download/Services/RegisterFetcher.cs
+++ Kartverket.Geonorge.Download/Services/RegisterFetcher.cs
@@ -106,6 +106,56 @@ namespace Kartverket.Geonorge.Download.Services
                     areas.Add(omraade);
                 }
 
+                //fylke old
+                url = System.Web.Configuration.WebConfigurationManager.AppSettings["RegistryUrl"] + "api/subregister/sosi-kodelister/kartverket/fylkesnummer";
+                c = new System.Net.WebClient();
+                c.Encoding = System.Text.Encoding.UTF8;
+                data = c.DownloadString(url);
+                response = Newtonsoft.Json.Linq.JObject.Parse(data);
+
+                codeList = response["containeditems"];
+
+                foreach (var code in codeList)
+                {
+                    var codevalue = code["label"].ToString();
+                    var label = code["description"].ToString();
+                    var status = code["status"].ToString();
+                    if (status == "Utgått")
+                        label = label + " (utgått)";
+                    else if (status == "Sendt inn")
+                        label = label + " (ny)";
+
+                    AreaType fylke = new AreaType { code = codevalue, name = label, type = "fylke" };
+                    var areaExists = areas.Where(a => a.code == codevalue && a.type == "fylke").FirstOrDefault();
+                    if(areaExists == null)
+                        areas.Add(fylke);
+                }
+
+                //kommune old
+                url = System.Web.Configuration.WebConfigurationManager.AppSettings["RegistryUrl"] + "api/subregister/sosi-kodelister/kartverket/kommunenummer";
+                data = c.DownloadString(url);
+                response = Newtonsoft.Json.Linq.JObject.Parse(data);
+
+                codeList = response["containeditems"];
+
+                foreach (var code in codeList)
+                {
+                    var codevalue = code["label"].ToString();
+                    var label = code["description"].ToString();
+                    var status = code["status"].ToString();
+
+                    if (status == "Utgått")
+                        label = label + " (utgått)";
+                    else if (status == "Sendt inn")
+                        label = label + " (ny)";
+
+                    AreaType kommune = new AreaType { code = codevalue, name = label, type = "kommune" };
+                    var areaExists = areas.Where(a => a.code == codevalue && a.type == "kommune").FirstOrDefault();
+                    if (areaExists == null)
+                        areas.Add(kommune);
+                }
+
+
                 memCacher.Add("areas", areas, new DateTimeOffset(DateTime.Now.AddHours(1)));
             }
 
