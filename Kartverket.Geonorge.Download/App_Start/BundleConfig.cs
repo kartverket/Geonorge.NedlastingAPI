4492ec9b5aafc415548885d6ab37fa7d62500951
Dag Olav Dahle <dagolav@arkitektum.no>
2018-08-22 13:44:58 +0200

    Update shared-partials components

--- Kartverket.Geonorge.Download/App_Start/BundleConfig.cs
+++ Kartverket.Geonorge.Download/App_Start/BundleConfig.cs
@@ -11,13 +11,20 @@ namespace Kartverket.Geonorge.Download
             bundles.Add(new StyleBundle("~/Content/bower_components/kartverket-felleskomponenter/assets/css/styles").Include(
                "~/Content/bower_components/kartverket-felleskomponenter/assets/css/vendor.min.css",
                "~/Content/bower_components/kartverket-felleskomponenter/assets/css/vendorfonts.min.css",
-               "~/Content/bower_components/kartverket-felleskomponenter/assets/css/main.min.css"
+               "~/Content/bower_components/kartverket-felleskomponenter/assets/css/main.min.css",
+                "~/dist/vendor.css",
+                "~/dist/main.css"
                ));
 
             bundles.Add(new ScriptBundle("~/Content/bower_components/kartverket-felleskomponenter/assets/js/scripts").Include(
                "~/Content/bower_components/kartverket-felleskomponenter/assets/js/vendor.min.js",
                "~/Content/bower_components/kartverket-felleskomponenter/assets/js/main.min.js"
                ));
+
+            bundles.Add(new ScriptBundle("~/shared-partials").Include(
+               "~/dist/vendor.js",
+               "~/dist/main.js"
+                ));
         }
     }
 }
