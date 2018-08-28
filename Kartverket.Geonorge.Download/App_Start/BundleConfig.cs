131d2be08cc6ae9084a6cdc64bdc9702b2c0be18
Dag Olav Dahle <dagolav@arkitektum.no>
2018-08-28 08:33:19 +0200

    Update components

--- Kartverket.Geonorge.Download/App_Start/BundleConfig.cs
+++ Kartverket.Geonorge.Download/App_Start/BundleConfig.cs
@@ -11,9 +11,12 @@ namespace Kartverket.Geonorge.Download
             bundles.Add(new StyleBundle("~/Content/bower_components/kartverket-felleskomponenter/assets/css/styles").Include(
                "~/Content/bower_components/kartverket-felleskomponenter/assets/css/vendor.min.css",
                "~/Content/bower_components/kartverket-felleskomponenter/assets/css/vendorfonts.min.css",
-               "~/Content/bower_components/kartverket-felleskomponenter/assets/css/main.min.css",
-                "~/dist/vendor.css",
-                "~/dist/main.css"
+               "~/Content/bower_components/kartverket-felleskomponenter/assets/css/main.min.css"
+               ));
+
+            bundles.Add(new StyleBundle("~/shared-partials-styles").Include(
+               "~/dist/vendor.css",
+               "~/dist/main.css"
                ));
 
             bundles.Add(new ScriptBundle("~/Content/bower_components/kartverket-felleskomponenter/assets/js/scripts").Include(
@@ -21,7 +24,7 @@ namespace Kartverket.Geonorge.Download
                "~/Content/bower_components/kartverket-felleskomponenter/assets/js/main.min.js"
                ));
 
-            bundles.Add(new ScriptBundle("~/shared-partials").Include(
+            bundles.Add(new ScriptBundle("~/shared-partials-scripts").Include(
                "~/dist/vendor.js",
                "~/dist/main.js"
                 ));
