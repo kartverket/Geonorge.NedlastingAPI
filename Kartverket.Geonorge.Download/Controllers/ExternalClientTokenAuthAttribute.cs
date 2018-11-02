9a301f003f4e430179b01505d946542021134622
Dag Olav Dahle <dagolav@arkitektum.no>
2018-11-02 09:24:39 +0100

    Log info

--- Kartverket.Geonorge.Download/Controllers/ExternalClientTokenAuthAttribute.cs
+++ Kartverket.Geonorge.Download/Controllers/ExternalClientTokenAuthAttribute.cs
@@ -24,7 +24,7 @@ namespace Kartverket.Geonorge.Download.Controllers.Api
 
             if (authorization == null || authorization.Scheme != "Bearer" || authorization.Parameter != externalClientToken)
             {
-                Log.Info("Authorization failed for client: invalid credentials");
+                Log.Info("Authorization failed for client: invalid credentials for request:" + context.Request);
                 context.ErrorResult = new AuthenticationFailureResult("Invalid credentials", context.Request);
             }
 
