c4a438ee1edfe7024dfb4c852a5b93dbbec12346
Dag Olav Dahle <dagolav@arkitektum.no>
2021-11-15 12:45:51 +0100

    Fix eiendomsfilter

--- Kartverket.Geonorge.Download/Services/ClipperService.cs
+++ Kartverket.Geonorge.Download/Services/ClipperService.cs
@@ -71,7 +71,7 @@ namespace Kartverket.Geonorge.Download.Services
 
                         if (!matrikkelEiendomAreas.Where(l => l.code == "0000").Any())
                         {
-                            var eiendomsQuery = eiendoms.Where(e => areaCodes.Contains(e.kommunenr));
+                            var eiendomsQuery = eiendoms.Where(e => areaCodes.Contains(e.kommunenr + "/" + e.gaardsnr + "/" + e.bruksnr + "/" + e.festenr));
                             eiendoms = eiendomsQuery.ToList();
                         }
 
