6c7aab29ef4fccc83fc170755a48f6014870f165
Dag Olav Dahle <dagolav@arkitektum.no>
2022-11-04 17:27:32 +0100

    Update frontend packages

--- Kartverket.Geonorge.Download/App_Start/BundleConfig.cs
+++ Kartverket.Geonorge.Download/App_Start/BundleConfig.cs
@@ -19,15 +19,10 @@ namespace Kartverket.Geonorge.Download
                "~/dist/main.css"
                ));
 
-            bundles.Add(new ScriptBundle("~/Content/bower_components/kartverket-felleskomponenter/assets/js/scripts").Include(
-               "~/Content/bower_components/kartverket-felleskomponenter/assets/js/vendor.min.js",
-               "~/Content/bower_components/kartverket-felleskomponenter/assets/js/main.min.js"
+            bundles.Add(new ScriptBundle("~/node-modules/scripts").Include(
+               "~/node_modules/@kartverket/geonorge-web-components/MainNavigation.js",
+               "~/node_modules/@kartverket/geonorge-web-components/GeoNorgeFooter.js"
                ));
-
-            bundles.Add(new ScriptBundle("~/shared-partials-scripts").Include(
-               "~/dist/vendor.js",
-               "~/dist/main.js"
-                ));
         }
     }
 }
